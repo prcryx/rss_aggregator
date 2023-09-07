d73342f16b29b90f2cc64778ffc0082195bc6f9f
prcryx <me.prc.fo@gmail.com>
2023-09-08 03:14:56 +0530

    error response function

--- common/utils/json.go
+++ common/utils/json.go
@@ -5,18 +5,28 @@ import (
 	"log"
 	"net/http"
 
-	cmn "github.com/prcryx/rss_aggregator/common"
 	constants "github.com/prcryx/rss_aggregator/common/constants"
+	errs "github.com/prcryx/rss_aggregator/common/errs"
 )
 
 func ResponseWithJSONData(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 	if err != nil {
-		log.Printf(cmn.FailedToMarshal)
-		w.WriteHeader(constants.InternalServerErrCode)
+		log.Printf(errs.FailedToMarshal)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	w.Header().Add(constants.ContentType, constants.ApplicationJson)
 	w.WriteHeader(code)
 	w.Write(data)
+}
+
+func ResponseWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("5xx Error: %v", msg)
+	}
+
+	ResponseWithJSONData(w, code, errs.ErrorResponse{
+		Error: msg,
+	})
 }
